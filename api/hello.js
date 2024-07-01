4ea91982b2c7e0f19ffdf0b0ba7b6cb8cad790a8
skycode <sikiruyaya1@gmail.com>
2024-07-01 16:40:19 +0100

    fetch the user information

--- api/hello.js
+++ api/hello.js
@@ -9,11 +9,11 @@ app.get('/api/hello', async (req, res) => {
   // Get the client's IP address
   const client_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-  // For demonstration purposes, we'll use a static location and temperature
+  // static location and temperature
   const location = 'New York';
   const temperature = 11; // Static temperature value
 
-  // Construct the greeting message
+  // greeting message
   const greeting = `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celsius in ${location}`;
 
   res.status(200).json({
